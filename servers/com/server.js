1c20e2eebe51224168e2c415e2f1405ea22bd1d2
opdykurihara <ykurihara@opendoor.co.jp>
2018-06-01 16:25:16 +0900

    labs/.comリポジトリ用server.jsでconnectSSIを利用しないものを作る/#9
    rewriteRulesで代用するserver.jsを追加。
    closing #9

--- servers/com/server.js
+++ servers/com/server.js
@@ -1,25 +1,22 @@
 /*
- .comリポジトリで使うserver.js
- Usage: npm i -g browser-sync connect-ssi
+ .com　メニューリポジトリで使うserver.js
+ Usage: npm i -g browser-sync connect-ssi path fs
 */
 const browserSync = require("browser-sync").create()
 const connectSSI = require("connect-ssi")
+const path = require("path")
+const fs = require("fs")
+const htdocs_locale_www = "D:/development/workspace/locale_www/htdocs_locale_www"
 
 browserSync.init({
   server: {
-    baseDir: [__dirname, "../"],
+    baseDir: ['../','../../'],
     routes: {
-      "/element/":  "D:/development/workspace/locale_www/htdocs_locale_www/element/",
-    },
-    middleware: [
-      connectSSI({
-        baseDir: __dirname,
-        ext: ".html"
-      })
-    ]
+      "/element/": htdocs_locale_www + "/element/",
+    }
   },
   // 監視するファイルと監視しないファイル
-  files: ["../**/*.css", "**/*.html", "../**/*.js", "!**/_dev/*"],
+  files: ["../../**/*.css", "**/*.html","../../**/*.js","!**/_dev/*"],
   notify: false,
   // 立ち上げ時のバスを指定したい場合
   // startPath: "/flights/",
@@ -31,4 +28,25 @@ browserSync.init({
   // port: 8888,
   // ブラウザを指定して立ち上げたい場合 ※Edgeだけうまく起動しない模様
   // browser: ["chrome","iexplore", "firefox", "microsoft-edge://"],
+  rewriteRules: [
+    {
+      match: /<!--#include virtual="(.+?)" -->/g,
+      fn(req, res, match, filename) {
+
+        let includeFilePath = path.join(__dirname, '../'+filename)
+
+        if(filename.match(/^\/(.*?)\//)[0] === "/_include/"){
+          includeFilePath = path.join(htdocs_locale_www + "/_cdg/", filename)
+        }
+
+        if (fs.existsSync(includeFilePath)) {
+            return fs.readFileSync(includeFilePath)
+        } else {
+            return `<span style="color: red">\`${includeFilePath}\` could not be found</span>`
+        }
+
+      }
+    }
+  ],
+
 })
